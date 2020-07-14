2cebdee228179821b445a435ab90a8b3b22061cf
Daniil Khlebovsky <danic42@mail.ru>
2020-07-14 18:51:48 +0500

    ready to run

--- khlebovsky/settings.py
+++ khlebovsky/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -64,7 +67,7 @@ WSGI_APPLICATION = 'khlebovsky.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
